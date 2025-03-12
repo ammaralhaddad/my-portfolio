663d123228500a50b4e05a99bde7e72f02b51c32
Ammar Haddad <alhaddadam@gmail.com>
2025-03-12 12:43:40 -0400

    first commit

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+
+function install_bootstrap(){
+
+
+     wp_enqueue_style('bootstrap-css', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css');
+     wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js', array('jquery'), null, true);
+     wp_enqueue_style('google-font' , "//fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Poppins:ital,wght@0,400;0,600;0,700;1,400;1,600&display=swap");
+     wp_enqueue_style('Font-Awsome', "//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
+     wp_enqueue_style('external_css', get_theme_file_uri('/src/index.css'));
+  
+}
+
+
+add_action('wp_enqueue_scripts', 'install_bootstrap' );
+
+?>
