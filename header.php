b23dd8ba1b30f429e737260e3d1cd44d5b30ac47
Ammar Haddad <alhaddadam@gmail.com>
2025-03-12 12:52:34 -0400

    added the navbar

--- header.php
+++ header.php
@@ -8,5 +8,33 @@
 </head>
 <body>
 
+<nav class="navbar navbar-expand-lg navbar-light ">
+    <div class="container">
+        <a class="navbar-brand" href="index.html"><i class='uil uil-user'></i>AMMAR AL HADDAD</a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto ">
+                <li class="nav-item">
+                    <a href="#" class="nav-link">About</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Skills</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Projects</a>
+                </li>
+                <li class="nav-item">
+                    <a href="#" class="nav-link">Contact Me</a>
+                </li>
+            </ul>
+
+            
+        </div>
+    </div>
+</nav>
 
 
