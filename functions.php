9536041e12196fa0c6dbe19c34defd60c463bceb
Ammar Haddad <alhaddadam@gmail.com>
2025-03-18 11:23:18 -0400

    move the CSS file to build folder

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ function install_bootstrap(){
      wp_enqueue_script('bootstrap-js', 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js', array('jquery'), null, true);
      wp_enqueue_style('google-font' , "//fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Poppins:ital,wght@0,400;0,600;0,700;1,400;1,600&display=swap");
      wp_enqueue_style('Font-Awsome', "//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css");
-     wp_enqueue_style('external_css', get_theme_file_uri('/src/index.css'));
+     wp_enqueue_style('external_css', get_theme_file_uri('/build/index.css'));
   
 }
 
